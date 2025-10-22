06036f383ff3b63370523921e9616fecf819344c
Dana Lenzig <danalenzig@gmail.com>
2025-10-22 12:32:13 +0200

    updatet MyApplication

--- Software-Engineering/firstProject/src/firstProject/MyApplication.java
+++ Software-Engineering/firstProject/src/firstProject/MyApplication.java
@@ -4,6 +4,7 @@ public class MyApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		System.out.println("My Name is Dana");
 		System.out.println(args);
 		// Ausgabe der Kommandozeilenparameter
 		for (int i=args.length-1; i>=0; i--)
